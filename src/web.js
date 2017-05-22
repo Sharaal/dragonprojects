f543856c77cb98864487683b8c3041d45281ee11
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-22 15:08:12 +0200

    Add vhost for www.maxdome-rssfeeds.de

--- src/web.js
+++ src/web.js
@@ -3,11 +3,15 @@ require('dotenv-safe').config();
 const app = require('express')();
 app.disable('x-powered-by');
 
-app.use('/maxdome-rssfeeds', require('@dragonprojects/maxdome-rssfeeds'));
+const vhost = require('vhost');
+
+const maxdomeRssfeeds = require('@dragonprojects/maxdome-rssfeeds');
+app.use('/maxdome-rssfeeds', maxdomeRssfeeds);
+app.use(vhost('www.maxdome-rssfeeds.de', maxdomeRssfeeds));
 
 const sharaal = require('@dragonprojects/sharaal');
 app.use('/sharaal', sharaal);
-app.use(require('vhost')('www.sharaal.de', sharaal));
+app.use(vhost('www.sharaal.de', sharaal));
 
 if (module.parent) {
   module.exports = app;
